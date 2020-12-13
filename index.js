edeb3407ac2c5d33e4ee741d3367b67a85ff6caf
Bluehold <75705512+Bluehold@users.noreply.github.com>
2020-12-13 19:58:13 +0900

    헤에
    v0.311b

--- index.js
+++ index.js
@@ -163,7 +163,7 @@ setInterval( function () {
   c.fillStyle = '#000'
   c.font = '20px Oswald';
   c.fillText('[제목 추천받음]', 1330,30);
-  c.fillText('v0.31b', 1330,60);
+  c.fillText('v0.311b', 1330,60);
   c.fillText('Tanks for Spotky1004', 1330,90);
 
   c.lineWidth = 5;
