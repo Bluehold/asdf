f5c947ef2e1af1c5f304f358caa1781dd6c5a7d0
Bluehold <75705512+Bluehold@users.noreply.github.com>
2020-12-13 12:16:57 +0900

    헤에
    v0.2b
    점프 개선
    공간 늘림
    기타 등등 ㅣㅎㅣㅎ

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var c = canvas.getContext('2d');
 
 var shapePosition = [100, 800];
 var g = 2.5;
-var obj = {};
+var gravity;
+var jumping = 0;
+var objA = {};
+var objB = {};
+var yplus = 0;
 class testses {
   constructor (data={}) {
     this.x = data.x || 0;
@@ -14,8 +18,8 @@ class testses {
     
   }
 }
-for (var i = 1; i < 51; i++) {
-  obj['shape' + i] = new testses({'x': 550, 'y': 250});
+for (var i = 1; i < 16; i++) {
+  objA['shape' + i] = new testses({'x': 550, 'y': 250});
 }
 
 function moveOutSine(shape, x2, y2, n, x1=shape.x, y1=shape.y, t=0) {
@@ -26,79 +30,79 @@ function moveOutSine(shape, x2, y2, n, x1=shape.x, y1=shape.y, t=0) {
 }
 function clock() {
   setTimeout( function() {
-  moveOutSine(obj.shape1,550,250,30);
-  moveOutSine(obj.shape2,550,350,30);
-  moveOutSine(obj.shape3,550,450,30);
-  moveOutSine(obj.shape4,550,550,30);
-  moveOutSine(obj.shape5,550,650,30);
-  moveOutSine(obj.shape6,550,250,30);
-  moveOutSine(obj.shape7,550,250,30);
-  moveOutSine(obj.shape8,550,450,30);
-  moveOutSine(obj.shape9,550,550,30);
-  moveOutSine(obj.shape10,550,650,30);
-  moveOutSine(obj.shape11,550,650,30);
-  moveOutSine(obj.shape13,550,350,30);
+  moveOutSine(objA.shape1,550,250,30);
+  moveOutSine(objA.shape2,550,350,30);
+  moveOutSine(objA.shape3,550,450,30);
+  moveOutSine(objA.shape4,550,550,30);
+  moveOutSine(objA.shape5,550,650,30);
+  moveOutSine(objA.shape6,550,250,30);
+  moveOutSine(objA.shape7,550,250,30);
+  moveOutSine(objA.shape8,550,450,30);
+  moveOutSine(objA.shape9,550,550,30);
+  moveOutSine(objA.shape10,550,650,30);
+  moveOutSine(objA.shape11,550,650,30);
+  moveOutSine(objA.shape13,550,350,30);
 }, 1000)
   setTimeout( function() {
-    moveOutSine(obj.shape9,550,450,1);
-    moveOutSine(obj.shape12,550,450,1);
+    moveOutSine(objA.shape9,550,450,1);
+    moveOutSine(objA.shape12,550,450,1);
   }, 1500)
   setTimeout( function() {
-  moveOutSine(obj.shape4,350,550,30);
-  moveOutSine(obj.shape6,450,250,30);
-  moveOutSine(obj.shape7,350,250,30);
-  moveOutSine(obj.shape8,450,450,30);
-  moveOutSine(obj.shape9,350,450,30);
-  moveOutSine(obj.shape10,450,650,30);
-  moveOutSine(obj.shape11,350,650,30);
-  moveOutSine(obj.shape12,350,450,30);
+  moveOutSine(objA.shape4,350,550,30);
+  moveOutSine(objA.shape6,450,250,30);
+  moveOutSine(objA.shape7,350,250,30);
+  moveOutSine(objA.shape8,450,450,30);
+  moveOutSine(objA.shape9,350,450,30);
+  moveOutSine(objA.shape10,450,650,30);
+  moveOutSine(objA.shape11,350,650,30);
+  moveOutSine(objA.shape12,350,450,30);
   }, 2000)
   setTimeout( function() {
-    moveOutSine(obj.shape4,550,550,30);
+    moveOutSine(objA.shape4,550,550,30);
   }, 3000)
   setTimeout( function() {
-  moveOutSine(obj.shape7,350,350,30);
-  moveOutSine(obj.shape6,350,250,30);
-  moveOutSine(obj.shape10,550,650,30);
-  moveOutSine(obj.shape11,550,650,30);
+  moveOutSine(objA.shape7,350,350,30);
+  moveOutSine(objA.shape6,350,250,30);
+  moveOutSine(objA.shape10,550,650,30);
+  moveOutSine(objA.shape11,550,650,30);
   }, 4000)
   setTimeout( function() {
-  moveOutSine(obj.shape2,350,350,30);
-  moveOutSine(obj.shape4,550,550,30);
-  moveOutSine(obj.shape6,450,250,30);
-  moveOutSine(obj.shape7,350,250,30);
-  moveOutSine(obj.shape10,450,650,30);
-  moveOutSine(obj.shape11,350,650,30);
-  moveOutSine(obj.shape13,350,350,30);
+  moveOutSine(objA.shape2,350,350,30);
+  moveOutSine(objA.shape4,550,550,30);
+  moveOutSine(objA.shape6,450,250,30);
+  moveOutSine(objA.shape7,350,250,30);
+  moveOutSine(objA.shape10,450,650,30);
+  moveOutSine(objA.shape11,350,650,30);
+  moveOutSine(objA.shape13,350,350,30);
   }, 5000)
   setTimeout( function() {
-    moveOutSine(obj.shape12,350,550,30);
+    moveOutSine(objA.shape12,350,550,30);
   }, 6000)
   setTimeout( function() {
-    moveOutSine(obj.shape2,550,350,30);
-    moveOutSine(obj.shape8,550,450,30);
-    moveOutSine(obj.shape9,550,450,30);
-    moveOutSine(obj.shape10,550,650,30);
-    moveOutSine(obj.shape11,550,650,30);
-    moveOutSine(obj.shape12,550,550,30);
-    moveOutSine(obj.shape13,550,350,30);
+    moveOutSine(objA.shape2,550,350,30);
+    moveOutSine(objA.shape8,550,450,30);
+    moveOutSine(objA.shape9,550,450,30);
+    moveOutSine(objA.shape10,550,650,30);
+    moveOutSine(objA.shape11,550,650,30);
+    moveOutSine(objA.shape12,550,550,30);
+    moveOutSine(objA.shape13,550,350,30);
   }, 7000)
   setTimeout( function() {
-    moveOutSine(obj.shape4,350,550,30);
-    moveOutSine(obj.shape6,450,250,30);
-    moveOutSine(obj.shape7,350,250,30);
-    moveOutSine(obj.shape8,450,450,30);
-    moveOutSine(obj.shape9,350,450,30);
-    moveOutSine(obj.shape10,450,650,30);
-    moveOutSine(obj.shape11,350,650,30);
-    moveOutSine(obj.shape13,350,350,30);
+    moveOutSine(objA.shape4,350,550,30);
+    moveOutSine(objA.shape6,450,250,30);
+    moveOutSine(objA.shape7,350,250,30);
+    moveOutSine(objA.shape8,450,450,30);
+    moveOutSine(objA.shape9,350,450,30);
+    moveOutSine(objA.shape10,450,650,30);
+    moveOutSine(objA.shape11,350,650,30);
+    moveOutSine(objA.shape13,350,350,30);
   }, 8000)
   setTimeout( function() {
-    moveOutSine(obj.shape4,550,550,30);
+    moveOutSine(objA.shape4,550,550,30);
   }, 9000)
   setTimeout( function() {
-    moveOutSine(obj.shape9,350,550,30);
-    moveOutSine(obj.shape8,350,450,30);
+    moveOutSine(objA.shape9,350,550,30);
+    moveOutSine(objA.shape8,350,450,30);
     clock();
   }, 10000)
 }
@@ -106,25 +110,43 @@ clock();
 
 function jumpdae() {
   setTimeout( function() {
-    moveOutSine(obj.shape14,250,800,10);
-    moveOutSine(obj.shape15,250,750,50);
+    moveOutSine(objA.shape14,250,800,10);
+    moveOutSine(objA.shape15,250,750,50);
   }, 1000)
   setTimeout( function() {
-    moveOutSine(obj.shape14,250,750,50);
-    moveOutSine(obj.shape15,250,800,10);
+    moveOutSine(objA.shape14,250,750,50);
+    moveOutSine(objA.shape15,250,800,10);
     jumpdae();
   }, 2000)
 }
 jumpdae();
 
+Math.rad = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+for (var i = 1; i < 16; i++) {
+  objB['shape' + i] = new testses({'x': 1000, 'y': 450 + yplus});
+  yplus += 25
+}
+
+/*setInterval(function() {
+  for(var Bmove in objB) {
+    var center = [1000, 450];
+    var dist = Math.sqrt((objB[Bmove].x-center[0])**2+(objB[Bmove].y-center[1])**2);
+    var centerDeg = (Math.atan2(objB[Bmove].y-center[1], objB[Bmove].x-center[0])/Math.PI*180+270+1)%360;
+    objB[Bmove] = [-Math.sin(Math.rad(centerDeg))*dist, Math.cos(Math.rad(centerDeg))*dist];
+  }
+},1)
+*/
+
 setInterval( function () {
-  c.clearRect(0,0,900,900)
+  c.clearRect(0,0,1500,900)
 
   c.lineWidth = 11;
   c.strokeStyle = '#000'
   c.fillStyle = '#bbbbbb'
   c.beginPath();
-  c.rect(0,0,900,900)
+  c.rect(0,0,1500,900)
   c.stroke();
   c.closePath();
 
@@ -133,6 +155,11 @@ setInterval( function () {
   c.font = '100px Oswald';
   c.textBaseline = 'middle';
   c.fillText(x + ' , ' + y, canvas.width/2-c.measureText(x + ' , ' + y).width/2, canvas.height/2);
+  c.fillStyle = '#000'
+  c.font = '20px Oswald';
+  c.fillText('[제목 추천받음]', 1330,30);
+  c.fillText('v0.2b', 1330,60);
+  c.fillText('Tanks for Spotky1004', 1330,90);
 
   c.lineWidth = 5;
   c.strokeStyle = '#0a88fe'
@@ -142,33 +169,66 @@ setInterval( function () {
   c.stroke();
   c.fill();
 
+  if ((gravity > 4 && Math.abs(860 - shapePosition[1]) < 3) || (shapePosition[1] == 870 && !(keypress['38']))) {
+    jumping = 0;
+  }
+  for (var asdf in objA) {
+    if ((objA[asdf].y - shapePosition[1] >= 19 && objA[asdf].y - shapePosition[1] <= 24 && -50 <= (objA[asdf].x - shapePosition[0]) && (objA[asdf].x - shapePosition[0]) <= 20 ) && !(keypress['38'])) {
+      jumping = 0;
+    }
+  }
+  for (var asdf in objA) {
+    if (objA[asdf].y - shapePosition[1] >= 19 && objA[asdf].y - shapePosition[1] <= 24 && -50 <= (objA[asdf].x - shapePosition[0]) && (objA[asdf].x - shapePosition[0]) <= 20 ) {
+      g = 2.5;
+      shapePosition[1] = objA[asdf].y - 25;
+    }
+  }
+  for (var asdf in objB) {
+    if ((objB[asdf].y - shapePosition[1] >= 19 && objB[asdf].y - shapePosition[1] <= 24 && -50 <= (objB[asdf].x - shapePosition[0]) && (objB[asdf].x - shapePosition[0]) <= 20 ) && !(keypress['38'])) {
+      jumping = 0;
+    }
+  }
+  for (var asdf in objB) {
+    if (objB[asdf].y - shapePosition[1] >= 19 && objB[asdf].y - shapePosition[1] <= 24 && -50 <= (objB[asdf].x - shapePosition[0]) && (objB[asdf].x - shapePosition[0]) <= 20 ) {
+      g = 2.5;
+      shapePosition[1] = objB[asdf].y - 25;
+    }
+  }
+  if (keypress['38']) jumping = 1;
   if (keypress['37'] && shapePosition[0] > 10) shapePosition[0] -= 2;
-  if (keypress['38'] && shapePosition[1] > 10) shapePosition[1] -= 5;
-  if (keypress['39'] && shapePosition[0] < 870) shapePosition[0] += 2;
+  if (jumping == 1 && shapePosition[1] > 10)  shapePosition[1] -= 5;
+  if (keypress['39'] && shapePosition[0] < 1470) shapePosition[0] += 2;
   if (shapePosition[1] < 870) {
-    shapePosition[1] += 0.4*g**2;
+    gravity = 0.4*g**2;
+    shapePosition[1] += gravity;
     g += 10/1000;
   }
-  else g = 2.5;
+  else {
+    g = 2.5;
+    gravity = 0;
+  }
 
   if(shapePosition[1] > 870) shapePosition[1] = 870;
 
   c.strokeStyle = '#008844'
   c.fillStyle = '#44ff88'
 
-  for (var hehe in obj) {
+  for (var Adraw in objA) {
   c.beginPath();
-  c.rect(obj[hehe].x,obj[hehe].y,50,50);
+  c.rect(objA[Adraw].x,objA[Adraw].y,50,50);
   c.stroke();
   c.fill();
   c.closePath();
   } 
-  for (var asdf in obj) {
-    if (obj[asdf].y - shapePosition[1] >= 19 && obj[asdf].y - shapePosition[1] <= 24 && -50 <= (obj[asdf].x - shapePosition[0]) && (obj[asdf].x - shapePosition[0]) <= 20 ) {
-      g = 2.5;
-      shapePosition[1] = obj[asdf].y - 25;
-    }
-  }
+  c.strokeStyle = '#4466bb'
+  c.fillStyle = '#4488ff'
+  for (var Bdraw in objB) {
+    c.beginPath();
+    c.rect(objB[Bdraw].x,objB[Bdraw].y,50,50);
+    c.stroke();
+    c.fill();
+    c.closePath();
+    } 
 }, 1);
 
 keypress = {};
@@ -179,4 +239,5 @@ function keyUp(e) {
 }
 function keyDown(e) {
   keypress[e.keyCode] = false;
+  
 }
